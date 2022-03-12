2e8426608a4c6820eb8c06acfc141f05becd554b
ITOAR <itoar@outlook.com>
2022-03-13 01:47:03 +0800

    load lle

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use lle::{num_complex::Complex64, Evolver, LinearOp, LleSolver};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -26,20 +27,98 @@ pub fn greet() {
 
 #[wasm_bindgen]
 pub struct Worker {
-    property: Vec<f64>,
+    core: LleSolver<
+        f64,
+        [Complex64; 128],
+        lle::LinearOpAdd<(lle::DiffOrder, Complex64), (lle::DiffOrder, Complex64)>,
+    >,
+    shell: [f64; 128],
+    property: WorkerProperty,
+}
+
+#[wasm_bindgen]
+#[derive(Debug, Clone, Copy)]
+pub struct WorkerProperty {
+    pub alpha: f64,
+    pub pump: f64,
+    pub linear: f64,
+    pub record_step: u64,
+    pub simu_step: f64,
 }
 
 #[wasm_bindgen]
 impl Worker {
     pub fn new() -> Self {
-        Self {
-            property: (0..128).map(|x| x as f64).collect(),
+        const STEP_DIST: f64 = 8e-4;
+        const PUMP: f64 = 3.94;
+        const LINEAR: f64 = -0.0444;
+        const ALPHA: f64 = -5.;
+        use lle::LinearOp;
+        utils::set_panic_hook();
+        Worker {
+            core: LleSolver::new(
+                [Complex64::new(1., 0.); 128],
+                STEP_DIST,
+                (0, -(Complex64::i() * ALPHA + 1.)).add((2, -Complex64::i() * LINEAR / 2.)),
+                Box::new(|x: Complex64| Complex64::i() * x.norm_sqr())
+                    as Box<dyn Fn(Complex64) -> Complex64>,
+                Complex64::from(PUMP),
+            ),
+            shell: [1.; 128],
+            property: WorkerProperty {
+                alpha: ALPHA,
+                linear: LINEAR,
+                pump: PUMP,
+                record_step: 100,
+                simu_step: STEP_DIST,
+            },
         }
     }
-    pub fn get_property(&self) -> *const f64 {
-        self.property.as_ptr()
+    pub fn get_property(&self) -> WorkerProperty {
+        self.property
+    }
+    pub fn get_state(&self) -> *const f64 {
+        self.shell.as_ptr()
     }
     pub fn get_len(&self) -> usize {
-        self.property.len()
+        self.shell.len()
+    }
+    pub fn set_property(&mut self, property: String, value: f64) {
+        match property.as_str() {
+            "alpha" => {
+                self.property.alpha = value;
+                self.core.linear = (0, -(Complex64::i() * self.property.alpha + 1.))
+                    .add((2, -Complex64::i() * self.property.linear / 2.))
+                    .into()
+            }
+            "pump" => {
+                self.property.alpha = value;
+                self.core.constant = Complex64::from(value).into();
+            }
+            "linear" => {
+                self.property.linear = value;
+                self.core.linear = (0, -(Complex64::i() * self.property.alpha + 1.))
+                    .add((2, -Complex64::i() * self.property.linear / 2.))
+                    .into()
+            }
+            "record_step" => {
+                self.property.record_step = value as u64;
+            }
+            "simu_step" => {
+                self.property.simu_step = value;
+            }
+            s => {
+                log!("Unknown property {}", s);
+            }
+        }
+    }
+    pub fn tick(&mut self) {
+        log!("updating state in rust");
+        self.core.evolve_n(self.property.record_step);
+        self.shell
+            .iter_mut()
+            .zip(self.core.state().iter())
+            .for_each(|(a, b)| *a = b.re);
+        log!("updated state in rust");
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,106 +1,41 @@
-import { Worker } from "wasm-lle";
+import { Worker, WorkerProperty } from "wasm-lle";
 import { memory } from "wasm-lle/wasm_lle_bg"
-import {
-    Chart,
-    ArcElement,
-    LineElement,
-    BarElement,
-    PointElement,
-    BarController,
-    BubbleController,
-    DoughnutController,
-    LineController,
-    PieController,
-    PolarAreaController,
-    RadarController,
-    ScatterController,
-    CategoryScale,
-    LinearScale,
-    LogarithmicScale,
-    RadialLinearScale,
-    TimeScale,
-    TimeSeriesScale,
-    Decimation,
-    Filler,
-    Legend,
-    Title,
-    Tooltip,
-    SubTitle
-} from 'chart.js';
+import Plotly from 'plotly.js-dist-min'
 
-Chart.register(
-    ArcElement,
-    LineElement,
-    BarElement,
-    PointElement,
-    BarController,
-    BubbleController,
-    DoughnutController,
-    LineController,
-    PieController,
-    PolarAreaController,
-    RadarController,
-    ScatterController,
-    CategoryScale,
-    LinearScale,
-    LogarithmicScale,
-    RadialLinearScale,
-    TimeScale,
-    TimeSeriesScale,
-    Decimation,
-    Filler,
-    Legend,
-    Title,
-    Tooltip,
-    SubTitle
-);const input = document.getElementById("input");
+const input = document.getElementById("input");
 
 const worker = Worker.new();
 const getProperty = () => {
     let data = worker.get_property();
-    console.log("js get value", data, typeof (data));
     return data;
 };
-const setProperty = (value) => {
-    console.log("js set value", value, typeof (value));
-    worker.set_property(value);
+const setProperty = (name, value) => {
+    worker.set_property(name, value);
 };
 
-document.getElementById("but1").onclick = uploadValue;
+setProperty("record_step", 1000);
 
+const getData = () => {
+    let data = new Float64Array(memory.buffer, worker.get_state(), worker.get_len());
+    return data;
+}
 
-const labels = new Float64Array(memory.buffer, worker.get_property(), worker.get_len());
+Plotly.newPlot(document.getElementById("plot"), [{
+    y: getData()
+}], {
+    margin: { t: 0 }
+});
 
-const canvas = document.getElementById("plot");
-const ctx = canvas.getContext('2d');
-
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data:[1],
-    }]
-};
-data.datasets[0].data = new Float64Array(memory.buffer, worker.get_property(), worker.get_len());
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-};
 
 const plot = () => {
-    let chartStatus = Chart.getChart("plot");
-    if (chartStatus != undefined) {
-        chartStatus.destroy();
-    }
-    console.log(data);
-    const myChart = new Chart(ctx, config);
+    Plotly.react(document.getElementById("plot"), [{
+        y: getData()
+    }])
 }
 
-plot();
 
-function uploadValue() {
-    plot();
+const updateDraw = () => {
+    worker.tick();
+    plot()
 }
+document.getElementById("but1").onclick = updateDraw;
